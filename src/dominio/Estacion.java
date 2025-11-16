f513d9600b9359ec2fac71c2ca65370cb41c2365
Bruno Recagno <brunorecagno@gmail.com>
2025-11-16 19:23:38 -0300

    Arreglado el test y eliminada instanciacion de cola de espera en constructor de Estacion

--- src/dominio/Estacion.java
+++ src/dominio/Estacion.java
@@ -74,7 +74,7 @@ public class Estacion implements Comparable {
         this.barrio = barrio;
         this.capacidad = capacidad;
         this.anclajesOcupados = 0;
-        this.colaEsperaAlquiler = new ColaNodos<Alquiler>(capacidad);
+        this.colaEsperaAlquiler = null;
     }
 
     // Constructor para test
@@ -83,7 +83,7 @@ public class Estacion implements Comparable {
         this.barrio = barrio;
         this.capacidad = capacidad;
         this.anclajesOcupados = anclajesOcupados;
-        this.colaEsperaAlquiler = new ColaNodos<Alquiler>(capacidad);
+        this.colaEsperaAlquiler = null;
     }
         
     // Metodos
